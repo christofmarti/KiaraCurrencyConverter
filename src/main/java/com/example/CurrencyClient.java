ecc8f8bd03d97d524325b6f9543f8e04273522c1
Christof Marti <mach@zhaw.ch>
2015-01-06 14:20:05 +0100

    Concrete Implemntation of Client call

--- src/main/java/com/example/CurrencyClient.java
+++ src/main/java/com/example/CurrencyClient.java
@@ -23,6 +23,8 @@
  
  
 package com.example;
+import java.util.Date;
+
 import org.fiware.kiara.client.Connection;
 import org.fiware.kiara.Context;
 import org.fiware.kiara.Kiara;
@@ -39,26 +41,48 @@ public class CurrencyClient {
 	public static void main (String [] args) throws Exception {
 		System.out.println("CurrencyExchangeClient");
 		
-		java.lang.String from = "";
-		java.lang.String to = "";
+		java.lang.String from = "EUR";
+		java.lang.String to = "CHF";
 				
 		ExchangeRate ret = null;
 		
 		Context context = Kiara.createContext();
 		
-		Transport transport = context.createTransport("tcp://127.0.0.1:9090");
+        System.out.println("Get Exchange rate using FakeService");
+		Transport transport = context.createTransport("tcp://127.0.0.1:8080");
 		Serializer serializer = context.createSerializer("cdr");
-		
 		Connection connection = context.connect(transport, serializer);
-		CurrencyExchange client = connection.getServiceProxy(CurrencyExchangeClient.class);
-		
+		CurrencyExchange client = connection.getServiceProxy(CurrencyExchangeClient.class);		
 		try {
 			ret = client.lookup(from, to);
-				
-			// Print or check the operation result here
+			printExchangeRate(ret);
 		} catch (Exception ex) {
 			System.out.println("Exception: " + ex.getMessage());
-			return;
+			System.exit(-1);
 		}
+		System.out.println("Get Exchange rate using YahooService");
+		Connection yahooCon = context.connect("tcp://127.0.0.1:9090/?serialization=cdr");
+		CurrencyExchange yahooClient = yahooCon.getServiceProxy(CurrencyExchangeClient.class);
+        try {
+            ret = yahooClient.lookup(from, to);
+            printExchangeRate(ret);
+        } catch (Exception ex) {
+            System.out.println("Exception: " + ex.getMessage());
+            System.exit(-1);
+        }
+		System.exit(0);
+	}
+	
+	public static void printExchangeRate(ExchangeRate data) {
+	    if (data==null) {
+	        System.out.println("Exchange Rate not found");
+	        return;
+	    }
+	    Date date = new Date(data.getTimestamp());
+        System.out.printf("Exchange-Rate: %s to %s%n", data.getFrom(), data.getTo());
+        System.out.printf("Date         : %td.%tm.%ty%n", date, date, date);
+        System.out.printf("Exchange Rate: %.4f%n", data.getRate());
+        System.out.printf("Bid Rate     : %.4f%n", data.getBid());
+        System.out.printf("Ask Rate     : %.4f%n", data.getAsk());
 	}
 }
